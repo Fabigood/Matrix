e7d9e49dfa9071dd0030cb4f4ff08c07fa7b9202
Fabigood <fabio.gonzalez.fiallos@udla.edu.ec>
2023-11-21 15:18:58 -0500

    final

--- matrixen1.c
+++ matrixen1.c
@@ -5,21 +5,21 @@ int main()
 
     int n, m;
     printf("Ingrese el numero de filas  \n");
-    scanf("%d", &n);
+    scanf("%d", &n);    //se pide al usuario el tamana de la matrix
     printf("Ingrese el numero de filas  \n");
     scanf("%d", &m);
 
     int Matrix[n][m];
 
     for (int i = 0; i < n; i++)
-    {
+    {               //se explica para imprimir los valores en x
 
         printf("\n");
-        for (int j = 0; j < m; j++)
+        for (int j = 0; j < m; j++) //para imprimir valores en y
         {
             if (i == j)
             {
-                Matrix[i][j] = 1;
+                Matrix[i][j] = 1; //se utiliza eso para que se imprima en los puntos iguales de cordenada en i y j que es la diagonal principal 
             }
             else
             {
